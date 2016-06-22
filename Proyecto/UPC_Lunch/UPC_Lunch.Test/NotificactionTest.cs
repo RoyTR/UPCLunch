7161b86569c6471cd16babbe2e303638d248cd25
Roy Taza <roytazarojas@gmail.com>
2016-06-22 16:43:32 -0500

    Pruebas Unitarias actualizadas.

---
+++ Proyecto/UPC_Lunch/UPC_Lunch.Test/NotificactionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UPC_Lunch.Test
+{
+    [TestClass]
+    public class NotificactionTest
+    {
+        [TestMethod]
+        public void NotifRestaurantAvailableTablesTest()
+        {
+        }
+
+        [TestMethod]
+        public void NotifRestaurantUnavailableTablesTest()
+        {
+        }
+
+        [TestMethod]
+        public void NotifDishAvailableTablesTest()
+        {
+        }
+
+        [TestMethod]
+        public void NotifDishUnavailableTablesTest()
+        {
+        }
+
+        [TestMethod]
+        public void UserGetNotificationsTest()
+        {
+        }
+
+        [TestMethod]
+        public void UserReadNotificationTest()
+        {
+        }
+    }
+}
---
+++ Proyecto/UPC_Lunch/UPC_Lunch.Test/SignalRTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UPC_Lunch.Test
+{
+    [TestClass]
+    public class SignalRTest
+    {
+        [TestMethod]
+        public void CreateSignalRNotificationTest()
+        {
+        }
+
+        [TestMethod]
+        public void SendSignalRNotificationTest()
+        {
+        }
+
+        [TestMethod]
+        public void ReceiveSignalRNotificationTest()
+        {
+            Assert.Fail("Server error. Notification has not been sent.");
+        }
+    }
+}
